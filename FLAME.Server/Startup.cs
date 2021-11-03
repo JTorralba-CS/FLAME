6e3b94334f4b51b7c3c0b027d3b89b13579aaabe
Julius Torralba <Julius.Torralba@GMail.com>
2021-11-02 20:12:25 -0600

    Purge unused.

--- FLAME.Server/Startup.cs
+++ FLAME.Server/Startup.cs
@@ -1,4 +1,3 @@
-using FLAME.Server.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +27,6 @@ namespace FLAME.Server
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
             services.AddScoped<System.Net.Http.HttpClient>();
         }
 
