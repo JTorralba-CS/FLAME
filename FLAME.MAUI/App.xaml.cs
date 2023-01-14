0bfb177cca7a99eeeedc24fbd81f7d9e4fcd316a
Julius Torralba <Julius.Torralba@GMail.com>
2023-01-13 19:41:56 -0700

    Customize main page.

--- FLAME.MAUI/App.xaml.cs
+++ FLAME.MAUI/App.xaml.cs
@@ -8,5 +8,23 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var Desktop = base.CreateWindow(activationState);
+
+            const int Width =414;
+            const int Height = 896;
+
+            Desktop.MinimumWidth = Width;
+            Desktop.MinimumHeight = Height;
+            Desktop.MaximumWidth = Width;
+            Desktop.MaximumHeight = Height;
+
+            Desktop.X = 753;
+            Desktop.Y = 92;
+
+            return Desktop;
+        }
     }
 }
